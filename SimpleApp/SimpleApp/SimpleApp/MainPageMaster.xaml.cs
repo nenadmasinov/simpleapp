58c2930650561c777d82088f03380d63189ad55b
Nenad Masinov <nenad@blafsoft.com>
2018-06-19 12:23:19 +0200

    adding missing files

---
+++ SimpleApp/SimpleApp/SimpleApp/MainPageMaster.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public MainPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MainPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MainPageMenuItem> MenuItems { get; set; }
+            
+            public MainPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<MainPageMenuItem>(new[]
+                {
+                    new MainPageMenuItem { Id = 0, Title = "Page 1" },
+                    new MainPageMenuItem { Id = 1, Title = "Page 2" },
+                    new MainPageMenuItem { Id = 2, Title = "Page 3" },
+                    new MainPageMenuItem { Id = 3, Title = "Page 4" },
+                    new MainPageMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ SimpleApp/SimpleApp/SimpleApp/SimplePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SimplePage : ContentPage
+	{
+        public int PageNumber = 0;
+        public SimplePage()
+        {
+            InitializeComponent();
+        }
+
+		public SimplePage (int pageNumber)
+		{
+            InitializeComponent();
+            PageNumber = pageNumber;
+            txtPageNumber.Text = PageNumber.ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var pgNumber = PageNumber + 1;
+            Navigation.PushAsync(new SimplePage(pgNumber));
+        }
+    }
+}
